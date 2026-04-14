03d04aa270cfa23a4515a394157552cebe0d6555
Gabriel Aversa Escobar <gea190307@gmail.com>
2026-04-14 17:19:04 -0300

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -2,29 +2,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Separamos os imports do Auth:
-import { getAuth, initializeAuth } from 'firebase/auth';
 
-// Colocamos o silenciador EXATAMENTE acima da palavra que está dando erro
+import { Auth, getAuth, initializeAuth } from 'firebase/auth';
+
 // @ts-ignore
 import { getReactNativePersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3XMc8t-w0rrriBjdaSu7POKfGbh9thb8",
+  // ATENÇÃO: Cole a sua apiKey verdadeira aqui dentro das aspas!
+  apiKey: "SUA_CHAVE_API_CORRETA_AQUI", 
   authDomain: "report-c3070.firebaseapp.com",
   projectId: "report-c3070",
   storageBucket: "report-c3070.firebasestorage.app",
   messagingSenderId: "1092143546861",
   appId: "1:1092143546861:web:29f582df4fb1940bed27e4",
 };
-// ... seus imports e sua const firebaseConfig lá em cima ...
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-let auth;
+let auth: Auth; 
+
 try {
   auth = initializeAuth(app, {
-    // @ts-ignore - Ignora o falso erro de tipagem do Firebase com o Expo
     persistence: getReactNativePersistence(AsyncStorage),
   });
 } catch (e) {
